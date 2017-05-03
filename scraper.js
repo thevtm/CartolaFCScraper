0936025481617313388a5598c2bb954430ded489
Vinícius Tabille Manjabosco <thevtm@gmail.com>
2017-05-03 15:12:22 -0300

    Cabeçario reduzido ao minimo

--- scraper.js
+++ scraper.js
@@ -16,9 +16,6 @@ const API_PARTIDAS = 'https://api.cartolafc.globo.com/partidas'
 const API_CLUBES = 'https://api.cartolafc.globo.com/clubes'
 
 const headers = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-  'Accept-Encoding': 'gzip, deflate, sdch, br',
-  'Accept-Language': 'en-US,en;q=0.8',
   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
 }
 
